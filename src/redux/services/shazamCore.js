09dd770332a67754f71857c0a722e3ed2d785125
Harshita184Rawat <harshita.reenu184@gmail.com>
2024-07-15 16:36:15 +0530

    Shazam core API setup

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,1 +1,24 @@
 {/* API calls */ }
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core7.p.rapidapi.com',
+        prepareHeaders: (headers) => {
+            headers.set('x-rapidapi-key', '1a4b7b0596msh6baea09fcf2159bp1c827bjsndc97d914606e');
+
+            return headers;
+
+        },
+    }),
+
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: () => 'charts/get-top-songs-in-world?limit=10' }),
+    }),
+
+});
+
+export const {
+    useGetTopChartsQuery, } = shazamCoreApi;
+
